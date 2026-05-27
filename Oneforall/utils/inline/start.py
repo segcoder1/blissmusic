dc5b62a9b97a2c7b60f93d976401b6f7a9837f0b
uppermooninfinity <uppermooninfinity@gmail.com>
2026-05-27 14:44:14 +0530

    Update start.py

--- Oneforall/utils/inline/start.py
+++ Oneforall/utils/inline/start.py
@@ -33,7 +33,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="･ʙᴏᴛ | ʏᴛ-ᴀᴘɪ ɪɴғᴏ･", callback_data="bot_info_data"),
         ],
     ]
     return buttons
