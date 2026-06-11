ebeb706f768672ca23414766f7c70afe10731cca
UCHIHABOTS <txkuze@gmail.com>
2026-06-11 11:52:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/uppermooninfinity/Kiyomimusic",
+    "https://github.com/segcoder1/blissmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
