639f24dfc1fef9b00b7172d05595c67e47acc006
UCHIHABOTS <txkuze@gmail.com>
2026-06-12 22:41:35 +0530

    Update start.py

--- Oneforall/utils/inline/start.py
+++ Oneforall/utils/inline/start.py
@@ -7,9 +7,6 @@ from Oneforall import app
 def start_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
@@ -19,11 +16,8 @@ def start_panel(_):
 def private_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true")
-        ],
-        [
+            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true"),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_5"], url="https://t.me/deafen_ackerman"),
         ],
         [
             InlineKeyboardButton(text="☁️sᴏᴜʀᴄᴇ☁️",callback_data="repo"),
